fdea6d8afed1862a5efa1fda7b4b7f1666f56f6d
omelette-bio <omelettebio@proton.me>
2025-05-24 14:51:15 +0200

    fin de projet ? aller cette fois c'est la bonne

--- Simulation/Elevator_dll.c
+++
@@ -1,2 +0,0 @@
-const char*  pszDllPathname = "Proof__Proof.dll";
-const char*  pszLauncherPathname = "C:/Program Files/ANSYS Inc/v241/SCADE Student/SCADE/bin/SCSSMLNC.exe";
--- Simulation/Elevator_mapping.h
+++
@@ -1,1 +0,0 @@
-/* Elevator_mapping.h */
